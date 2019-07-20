7010d163e7f647a2617d7145a612f5ee8f277457
quq99 <eequq99@gmail.com>
2019-07-20 10:24:04 -0400

    array: update problem 33_Search_in_Rotated_Sorted_Array

--- Array/medium/33_Search_in_Rotated_Sorted_Array/SearchinRotatedSrtArray.cpp
+++ Array/medium/33_Search_in_Rotated_Sorted_Array/SearchinRotatedSrtArray.cpp
@@ -32,3 +32,37 @@ class SearchinRotatedSrtArray {
 
         }
 };
+
+class SearchinRotatedSrtArray2 {
+    public:
+        int search(vector<int>& nums, int target) {
+            int len = nums.size();
+            if (len==0) return -1;
+            if (len==1) return nums[0]==target? 0 : -1;
+            int l = 0;
+            int r = len - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                else if (nums[mid] > target) {
+                    // mid position 1
+                    if (nums[mid] < nums[r]) r = mid-1;
+                    // mid position 2, two cases
+                    else if (target >= nums[l]) r = mid - 1;
+                    else l = mid + 1;
+                }
+                else {
+                    // mid position 1
+                    if (nums[mid] > nums[r]) l = mid + 1;
+                    // mid position 2, two cases
+                    else if (target <= nums[r]) l = mid + 1;
+                    else r = mid - 1;
+                }
+            }
+
+
+            return -1;
+        }
+};
